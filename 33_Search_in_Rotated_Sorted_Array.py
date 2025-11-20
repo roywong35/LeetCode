2ea407576229634c2c98fe83bead97762eaf3560
roywong35 <wroy123@gmail.com>
2025-11-20 16:39:12 +0900

    Completed one question

---
+++ 33_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,19 @@
+def search(nums: list[int], target: int) -> int:
+    low, high = 0, len(nums) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target < nums[mid]:
+                high = mid -1
+            else:
+                low = mid + 1
+        else:
+            if nums[mid] < target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+    return -1
+print(search(nums = [4,5,6,7,0,1,2], target = 0))
